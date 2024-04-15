8c8a75fcf69cad5e31c2bf6f7599aa53c26235f0
lhg010423 <lhg010423@gmail.com>
2024-04-15 16:11:44 +0900

    240415

---
+++ boardProject-boot/src/main/java/edu/kh/project/myPage/model/mapper/MyPageMapper.java
@@ -0,0 +1,38 @@
+package edu.kh.project.myPage.model.mapper;
+
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import edu.kh.project.member.model.dto.Member;
+
+@Mapper // mybatis에서 mapper인것을 인식시키는거고 interface이다
+public interface MyPageMapper {
+
+	/** 회원 정보 수정
+	 * @param inputMember
+	 * @return result
+	 */
+	int updateInfo(Member inputMember);
+
+	/** 회원의 비밀번호 조회
+	 * @param memberNo
+	 * @return 암호화된 비밀번호
+	 */
+	String selectPw(int memberNo);
+
+	/** 비밀번호 변경
+	 * @param paramMap
+	 * @return result
+	 */
+	int changePw(Map<String, Object> paramMap);
+
+	/** 회원 탈퇴
+	 * @param memberNo
+	 * @return result
+	 */
+	int secession(int memberNo);
+	
+	
+	
+}
