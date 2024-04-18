cf7a98926dc34441b38fe61f2403461f3fe57c9b
lhg010423 <lhg010423@gmail.com>
2024-04-18 17:45:55 +0900

    240418

--- boardProject-boot/src/main/java/edu/kh/project/main/controller/MainController.java
+++ boardProject-boot/src/main/java/edu/kh/project/main/controller/MainController.java
@@ -1,13 +1,9 @@
 package edu.kh.project.main.controller;
 
-import java.util.List;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import edu.kh.project.member.model.dto.Member;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class MainController {
@@ -20,6 +16,14 @@ public class MainController {
 	}
 	
 	
+	// LoginFilter -> loginError 리다이렉트
+	// -> message 만들어서 메인페이지로 리다이렉트
+	@GetMapping("loginError")
+	public String loginError(RedirectAttributes ra) {
+		
+		ra.addFlashAttribute("message", "로그인 후 이용해 주세요");
+		return "redirect:/";
+	}
 	
 	
 }
